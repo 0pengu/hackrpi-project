a7d4a339aab1dde6e8130a44297b0b7c38696112
Tahmid Ahmed <tahmid@midhat.io>
2024-11-10 07:07:27 -0500

    Added some more styles to leaflet DOM items

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -7,9 +7,9 @@ export default function RootPage() {
     <>
       <Map />
       <motion.div
-        initial={{ bottom: -16, opacity: 0, filter: "blur(10px)" }}
-        animate={{ bottom: 8, opacity: 1, filter: "blur(0px)" }}
-        transition={{ delay: 0.25, duration: 1, ease: "easeInOut" }}
+        initial={{ left: "-100px", opacity: 0, filter: "blur(10px)" }}
+        animate={{ left: 0, opacity: 1, filter: "blur(0px)" }}
+        transition={{ duration: 2, ease: "easeInOut" }}
         className="fixed left-2 bottom-2 z-[999999999999] rounded-lg backdrop-blur-lg px-4 py-2"
       >
         <motion.img
