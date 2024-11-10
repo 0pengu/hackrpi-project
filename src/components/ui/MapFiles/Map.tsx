4dc776a75aecc49487f42005fea9b7aca7e44a35
haokingluo <hao5k6032@gmail.com>
2024-11-10 01:42:41 -0500

    Add heat map of protected, plazas, interest points
    Added to Map.tsx to generate heat maps of protected intersections,
    plazas, and interest points.

--- src/components/ui/MapFiles/Map.tsx
+++ src/components/ui/MapFiles/Map.tsx
@@ -38,7 +38,8 @@ function MapChild({ start, end }: { start: LatLng; end: LatLng }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const allPoints = [];
+  const allPoints = []; // each element is [lat, long, intensity]
+  // const allPolygons = []; // each element is a LatLngBound
 
   useEffect(() => {
     const fetchNYCData = async () => {
@@ -49,36 +50,43 @@ function MapChild({ start, end }: { start: LatLng; end: LatLng }) {
         },
       };
 
-      // const query = "?$where=cmplnt_fr_dt >= '2024-09-29T00:00:00'";
-      // const response = await fetch(
-      //   `https://data.cityofnewyork.us/resource/5jvd-shfj.json${query}`,
-      //   fetchOptions
-      // );
+      // Protected intersxns (high bias), return multipoint
+      const response = await fetch('https://data.cityofnewyork.us/resource/bryy-vqd9.json', fetchOptions);
 
-      const fetchURLs = [
-        'https://data.cityofnewyork.us/resource/bryy-vqd9.json', // protected intersxns
-        'https://data.cityofnewyork.us/resource/k5k6-6jex.json', // pedestrian plazas
-      ]
+      if (!response.ok) throw new Error("fetch failed");
+      const datum = await response.json();
 
-      const dataJsons = [];
-
-      for (const url in fetchURLs){
-        const res = await fetch(url, fetchOptions);
-        if (!res.ok ) throw new Error("fetch failed");
-        const data = await res.json();
-
-        dataJsons.push(data);
-      }
-
-      for (const point of dataJsons[0]) {
-        // console.log("Point:" , point.the_geom.coordinates[0]);
-        // console.log("..", [point.the_geom.coordinates[0][0], point.the_geom.coordinates[0][1], 0.7]);
-        // allPoints.push([point.latitude, point.longitude, 0.7]);
-
+      for (const point of datum) {
         // store as [lat, long, intensity]
         // except retrieve it as [long, lat]
         allPoints.push([point.the_geom.coordinates[0][1], point.the_geom.coordinates[0][0], 0.7])
       }
+
+      // Plazas (medium bias), return multipoly
+      const res = await fetch('https://data.cityofnewyork.us/resource/k5k6-6jex.json', fetchOptions);
+      if (!res.ok) throw new Error("fetch failed");
+      const data = await res.json();
+
+      for (const f of data) {
+        console.log("Point:" , f.the_geom.coordinates.flat(Infinity)); // lol
+        // Now each f is an array of points, pairs of long and lat for a single point (retrieved long, lat)
+        const bruh = f.the_geom.coordinates.flat(Infinity);
+        for (let i = 0; i < bruh.length-1; i+=2){
+          // console.log("Pushing this: ", [bruh[i+1], bruh[i], 0.1]);
+          allPoints.push([bruh[i+1], bruh[i], 0.4]);
+        }
+      }
+
+      // Points of interest (low bias), return singular point
+      const RES = await fetch('https://data.cityofnewyork.us/resource/t95h-5fsr.json', fetchOptions);
+      if (!RES.ok) throw new Error("fetched failed");
+      const DATA = await RES.json();
+      for (const POINT of DATA){
+        console.log("POINT: ", POINT.the_geom.coordinates); // long, lat
+        allPoints.push([POINT.the_geom.coordinates[1], POINT.the_geom.coordinates[0], 0.1]);
+      }
+
+
     };
     fetchNYCData();
   }, []);
